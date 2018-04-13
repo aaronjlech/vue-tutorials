d0637b4de4513c40567458f4fdd4395977b86f77
Aaron LeCheminant <aaronjlech@gmail.com>
2018-04-13 13:02:36 -0400

    add lifecycle method

--- vuejs-poster-shop/public/script.js
+++ vuejs-poster-shop/public/script.js
@@ -4,25 +4,33 @@ new Vue({
         total: 0,
         items: [],
         cart: [],
-        search: '',
+        search: 'dinosaurs',
+        lastSearch: '',
+        loading: true,
+    },
+    mounted: function() {
+        this.onSubmit();
     },
     methods: {
         onSubmit: function() {
+            this.loading = true;
             this.$http.get(`/search/${this.search}`)
                 .then(function(res) {
+                    this.loading = false;
+                    this.lastSearch = this.search;
+                    this.search = "";
                     this.items = res.data.map(imgur => {
                         imgur.price = Math.floor(Math.random() * 30) + 6;
                         return imgur;
                     })
                 })
         },
-        addItem: function(itemToAdd) {
+        addItemToCart: function(itemToAdd) {
             let itemInCart = this.cart.find(item => item.id === itemToAdd.id);
             if (itemInCart) {
                 itemInCart.qty += 1;
                 // this.cart = this.cart.map(item => item.id === itemInCart.id ? { ...item, qty: item.qty += 1 } : item );
             } else {
-
                 this.cart.push({
                     ...itemToAdd,
                     qty: 1
