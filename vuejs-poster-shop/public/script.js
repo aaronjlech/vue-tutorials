53118b9acc9a4804fff437fe264f9a70d95d119b
Aaron LeCheminant <aaronjlech@gmail.com>
2018-04-13 12:28:00 -0400

    rendering teh data

--- vuejs-poster-shop/public/script.js
+++ vuejs-poster-shop/public/script.js
@@ -1,1 +1,42 @@
-console.log('It works.');
+new Vue({
+    el: '#app',
+    data: {
+        total: 0,
+        items: [],
+        cart: []
+    },
+    methods: {
+        addItem: function(itemToAdd) {
+            let itemInCart = this.cart.find(item => item.id === itemToAdd.id);
+            if (itemInCart) {
+                itemInCart.qty += 1;
+                // this.cart = this.cart.map(item => item.id === itemInCart.id ? { ...item, qty: item.qty += 1 } : item );
+            } else {
+
+                this.cart.push({
+                    ...itemToAdd,
+                    qty: 1
+                })
+            }
+            this.setCartTotal();
+        },
+        setCartTotal: function() {
+            let newTotal = 0;
+            this.cart.forEach(item => newTotal += (item.price * item.qty));
+            this.total = newTotal;
+        },
+        updateQty: function(item, qty) {
+            if(qty == 0) {
+                this.cart = this.cart.filter(cartItem => cartItem.id !== item.id);
+            }
+            if(qty > 0) {
+                item.qty = qty;
+            }
+        }
+    },
+    filters: {
+        currency: function(price) {
+            return `$${price}`;
+        }
+    }
+})
